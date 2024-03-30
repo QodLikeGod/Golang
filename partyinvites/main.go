ea186eea58d01a9fbad7cbcc8e277c9c3a1a0a10
Roma Shmak <ShmakovRK@protonmail.com>
2024-03-31 02:44:25 +0300

    final

--- partyinvites/main.go
+++ partyinvites/main.go
@@ -5,10 +5,14 @@ import (
 	"html/template"
 	"net/http"
 )
+
+// partyinvites
+
 type Rsvp struct {
 	Name, Email, Phone string
-	WillAttend bool
+	WillAttend         bool
 }
+
 var responses = make([]*Rsvp, 0, 10)
 var templates = make(map[string]*template.Template, 3)
 
@@ -40,19 +44,50 @@ type formData struct {
 
 func formHandler(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == http.MethodGet {
-		templates["form"].Execute(writer, formData {
+		templates["form"].Execute(writer, formData{
 			Rsvp: &Rsvp{}, Errors: []string{},
 		})
-	}
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		responseData := Rsvp{
+			Name:       request.Form["name"][0],
+			Email:      request.Form["email"][0],
+			Phone:      request.Form["phone"][0],
+			WillAttend: request.Form["willattend"][0] == "true",
+		}
 
+		errors := []string{}
+		if responseData.Name == "" {
+			errors = append(errors, "Please enter your name")
+		}
+		if responseData.Email == "" {
+			errors = append(errors, "Please enter your email address")
+		}
+		if responseData.Phone == "" {
+			errors = append(errors, "Please enter your phone number")
+		}
+		if len(errors) > 0 {
+			templates["form"].Execute(writer, formData{
+				Rsvp: &responseData, Errors: errors,
+			})
+		} else {
+			responses = append(responses, &responseData)
+			if responseData.WillAttend {
+				templates["thanks"].Execute(writer, responseData.Name)
+			} else {
+				templates["sorry"].Execute(writer, responseData.Name)
+			}
+		}
+	}
 }
+
 func main() {
 	loadTemplates()
 	http.HandleFunc("/", welcomeHandler)
 	http.HandleFunc("/list", listHandler)
 	http.HandleFunc("/form", formHandler)
 	err := http.ListenAndServe(":5000", nil)
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(err)
-}
+	}
 }
